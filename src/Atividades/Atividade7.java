1a149d4dec8671f794b40c54998d4fcd0e6c6a68
a3ternus <luanvitorlima2017@gmail.com>
2025-01-26 08:21:35 -0300

    operadores comparativos

---
+++ src/Atividades/Atividade7.java
@@ -0,0 +1,16 @@
+package Atividades;
+
+public class Atividade7 {
+	
+	/*
+	 * Operadores comparativos:
+	 * 
+	 * >  	Maior
+	 * <  	Menor
+	 * >=	Maior ou igual a
+	 * <= 	Menor ou igual a
+	 * ==	Igual
+	 * != 	Diferente
+	 * 
+	 * */
+}
