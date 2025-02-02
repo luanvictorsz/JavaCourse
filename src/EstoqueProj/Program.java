d69788e2a024eb86cfff1c5e3334eaeff9aafb84
a3ternus <luanvitorlima2017@gmail.com>
2025-02-01 23:20:04 -0300

    adicionando o metodo de adicionar e remover do estoque no programa

--- src/EstoqueProj/Program.java
+++ src/EstoqueProj/Program.java
@@ -18,6 +18,18 @@ public class Program {
 		
 		System.out.println(p);
 		
+		System.out.println("Enter the number of products to be added in stock: ");
+		int qtdAdd = sc.nextInt();
+		p.AddInStock(qtdAdd);
+		System.out.println("Updated data:");
+		System.out.println(p);
+		
+		System.out.println("Enter the number of products to be removed from stock: ");
+		int qtdRemove = sc.nextInt();
+		p.RemoveInStock(qtdRemove);
+		System.out.println("Updated data:");
+		System.out.println(p);
+		
 		sc.close();
 	
 	}
