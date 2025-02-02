2859d7d713130695c7755cc375a619b708d15723
a3ternus <luanvitorlima2017@gmail.com>
2025-02-02 20:40:49 -0300

    organizando impressao

--- src/EstoqueProj/Program.java
+++ src/EstoqueProj/Program.java
@@ -21,14 +21,14 @@ public class Program {
 		System.out.println("Enter the number of products to be added in stock: ");
 		int qtdAdd = sc.nextInt();
 		p.AddInStock(qtdAdd);
-		System.out.println("Updated data:");
-		System.out.println(p);
+		System.out.println();
+		System.out.println("Updated data: " + p);
 		
 		System.out.println("Enter the number of products to be removed from stock: ");
 		int qtdRemove = sc.nextInt();
 		p.RemoveInStock(qtdRemove);
-		System.out.println("Updated data:");
-		System.out.println(p);
+		System.out.println();
+		System.out.println("Updated data: " + p);
 		
 		sc.close();
 	
