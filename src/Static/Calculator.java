95f247b8c20e2fd5764414784638bb4e7374d042
a3ternus <luanvitorlima2017@gmail.com>
2025-02-07 21:43:28 -0300

    adicionando comentários sobre a aula anterior

--- src/Static/Calculator.java
+++ src/Static/Calculator.java
@@ -2,8 +2,12 @@ package Static;
 
 public class Calculator {
 	
+
+	//Pi é um valor estatico imutavel
+	
 	public static double PI = 3.14159;
 	
+	//a Circuferencia tambem possui um valor estatico 
 	public static double Circumference(double radius)
 	{
 		return 2.0 * PI * radius; 
@@ -13,4 +17,6 @@ public class Calculator {
 	{
 		 return 4.0 * PI * radius * radius / 3.0; 
 	}
+	
+	//obs: eu posso chamar a classe direta sem precisar instanciar no nosso programa, os valores não serão modificados
 }
