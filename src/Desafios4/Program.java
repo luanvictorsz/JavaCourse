a10dc9336aae48ed614d0b801bb1baf84156787a
a3ternus <luanvitorlima2017@gmail.com>
2025-02-17 22:30:29 -0300

    exercicio resolvido

--- src/Desafios4/Program.java
+++ src/Desafios4/Program.java
@@ -12,17 +12,19 @@ public class Program {
 		System.out.println("Quantos numeros voce vai adicionar: ");
 		int n = sc.nextInt();
 		
+		int[] vet = new int[n];
+		
 		for(int i = 0; i < n; i++) {
-			System.out.println("Digite um número: ");
-			int num = sc.nextInt();
-			
-			if(num > 0) {
-				System.out.println("Numeros Negativos: ");
-				
-			}
+			System.out.println("Digite um numero: ");
+			vet[i] = sc.nextInt();
 		}
 		
-		
+		System.out.println("Numeros negativos: ");
+		for(int i = 0; i < n; i++) {
+			if(vet[i] < 0) {
+				System.out.println(vet[i]);
+			}
+		}
 		
 		sc.close();
 		
--- src/Package/Main.java
+++ src/Package/Main.java
@@ -6,7 +6,6 @@ import Desafios3.Program;
 public class Main {
 
 	public static void main(String[] args) {
-		Atividade014.Executar();
+		Desafios4.Program.Executar();
 	}
-
 }
