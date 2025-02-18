91c1ca23bfef5298d13a14f167eecd1aec4ae9b5
a3ternus <luanvitorlima2017@gmail.com>
2025-02-17 23:55:54 -0300

    calculando a porcentagem de idade

--- src/Desafios5/Program.java
+++ src/Desafios5/Program.java
@@ -29,12 +29,30 @@ public class Program {
 		for(int i = 0; i < n; i++) {
 			soma = soma + alturas[i];
 		}
+		
 		double mediaAlturas = soma / n;
 		
 		System.out.println();
-		System.out.printf("Altura média: %.2f%n" + mediaAlturas);
+		System.out.printf("Altura média: %.2f%n", mediaAlturas);
 
-		 
+		
+		int cont = 0;
+		for(int i = 0; i < 0; i++) {
+			if(idades[i] < 16){
+				cont = cont + 1;
+			}
+		}
+		
+		double percent = cont * 100.0 / n;
+		
+		System.out.printf("Pessoas com menos de 16 anos: %.1f%%%n", percent);
+		
+		for(int i = 0; i < 0; i++) {
+			if(idades[i] < 16){
+				System.out.println(nomes[i]);
+			}
+		}
+		
 		sc.close();
 	}
 }
