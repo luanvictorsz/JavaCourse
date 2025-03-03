54e7e10a53867ffd85d48e68b687f41fd065b1ed
victor <luanvitorlima2017@gmail.com>
2025-03-02 21:18:19 -0300

    trabalhando com vetores (refatorando atividade)

--- src/Atividades/Atividade018.java
+++ src/Atividades/Atividade018.java
@@ -13,20 +13,20 @@ public class Atividade018 {
 
         int[][] mat = new int[n][n];
 
-        for (int i = 0; i < mat.length; i++) {
+        for (int i = 0; i < mat[i].length; i++) {
             for (int j = 0; j < n; j++) {
                 mat[i][j] = sc.nextInt();
             }
         }
 
         System.out.println("Main Diagonal: ");
-        for(int i = 0; i < mat.length; i++){
+        for(int i = 0; i < mat[i].length; i++){
             System.out.print(mat[i][i] + " ");
         }
         System.out.println();
         int count = 0;
 
-        for(int i = 0; i < mat.length; i++){
+        for(int i = 0; i < mat[i].length; i++){
             for(int j = 0; j < n; j++) {
                 if (mat[i][j] < 0) {
                     count++;
