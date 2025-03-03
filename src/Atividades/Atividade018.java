ad406a122f19a521514d9ecb0ae617057d071566
victor <luanvitorlima2017@gmail.com>
2025-03-02 21:15:11 -0300

    trabalhando com vetores (mostrando valores negativos)

--- src/Atividades/Atividade018.java
+++ src/Atividades/Atividade018.java
@@ -4,8 +4,8 @@ import java.util.Locale;
 import java.util.Scanner;
 
 public class Atividade018 {
-    //Trabalhando com matrizes
-    public static void Executar(){
+
+    public static void Executar() {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
 
@@ -13,10 +13,29 @@ public class Atividade018 {
 
         int[][] mat = new int[n][n];
 
-        for(int i = 0; i < n; i++){
-            for(int j = 0; i < n; j++){
+        for (int i = 0; i < mat.length; i++) {
+            for (int j = 0; j < n; j++) {
                 mat[i][j] = sc.nextInt();
             }
         }
+
+        System.out.println("Main Diagonal: ");
+        for(int i = 0; i < mat.length; i++){
+            System.out.print(mat[i][i] + " ");
+        }
+        System.out.println();
+        int count = 0;
+
+        for(int i = 0; i < mat.length; i++){
+            for(int j = 0; j < n; j++) {
+                if (mat[i][j] < 0) {
+                    count++;
+                }
+            }
+        }
+        System.out.print("Negative Numbers: " + count);
+
+        sc.close();
     }
+
 }
