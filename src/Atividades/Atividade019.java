228e3f90580324dcf2735466c738525286f3bc04
victor <luanvitorlima2017@gmail.com>
2025-03-03 23:42:55 -0300

    trabalhando com data e hora pt-2

--- src/Atividades/Atividade019.java
+++ src/Atividades/Atividade019.java
@@ -14,5 +14,15 @@ public class Atividade019 {
         System.out.println(date01);
         System.out.println(date02);
         System.out.println(date03);
+
+        LocalDate data04 = LocalDate.parse("2025-03-03");
+        LocalDateTime data05 = LocalDateTime.parse("2025-03-03T01:30:26"); //obs: ver documentação de data e hora
+        Instant data06 = Instant.parse("2025-03-03T23:40:25Z");
+        Instant data07 = Instant.parse("2025-03-03T23:41:26-03:00");
+
+        System.out.println(data04);
+        System.out.println(data05);
+        System.out.println(data06);
+        System.out.println(data07);
     }
 }
