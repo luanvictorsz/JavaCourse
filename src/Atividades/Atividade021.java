97611f45c36c02c311483996d3d8b0302f0d79e5
victor <luanvitorlima2017@gmail.com>
2025-03-21 18:25:09 -0300

    usando utc de forma manual

--- src/Atividades/Atividade021.java
+++ src/Atividades/Atividade021.java
@@ -4,20 +4,24 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.time.Instant;
 import java.util.Date;
+import java.util.TimeZone;
 
 public class Atividade021 {
     public  static  void Executar() throws ParseException {
 
         SimpleDateFormat sdf1 = new SimpleDateFormat("dd/MM/yyyy");
         SimpleDateFormat sdf2 = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+        SimpleDateFormat sdf3 = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+        sdf3.setTimeZone(TimeZone.getTimeZone("GMT")); //definindo modelo UTC de forma manual
 
         Date y1 = sdf1.parse("19/03/2025");
         Date y2 = sdf2.parse("19/03/2025 20:00:24");
-        Date y3 = Date.from(Instant.parse("2025/03/21T18:07:53Z"));
+        Date y3 = Date.from(Instant.parse("2025-03-21T15:42:07Z"));
 
         System.out.println("y1: " + sdf1.format(y1));
         System.out.println("y2: " + sdf2.format(y2));
         System.out.println("y3: " + sdf2.format(y3)); //data formato UTC - A letra Z defini isso
+
 
         Date x1 = new Date();
         Date x2 = new Date(System.currentTimeMillis());
@@ -30,5 +34,15 @@ public class Atividade021 {
         System.out.println("Data e hora atual usando milissegundos: " + sdf2.format(x2));
         System.out.println("Data e hora com milissegundos zerados: " + sdf2.format(x3));
         System.out.println(sdf2.format(x4));
+
+        System.out.println("----------");
+        System.out.println("formato utc: " + sdf3.format(x1));
+        System.out.println("formato utc: " + sdf3.format(x2));
+        System.out.println("formato utc: " + sdf3.format(x3));
+        System.out.println("formato utc: " + sdf3.format(x4));
+        System.out.println("formato utc: " + sdf3.format(y1));
+        System.out.println("formato utc: " + sdf3.format(y2));
+        System.out.println("formato utc: " + sdf3.format(y3));
+
     }
 }
