158284bf8013b3877a5668eb88ddf6aa372e270b
victor <luanvitorlima2017@gmail.com>
2025-03-24 21:56:38 -0300

    trabalhando com calendar, atualizando a data atual incrementando dias com o metedo add

--- src/Atividades/Atividade022.java
+++ src/Atividades/Atividade022.java
@@ -2,14 +2,21 @@ package Atividades;
 
 import java.text.SimpleDateFormat;
 import java.time.Instant;
+import java.util.Calendar;
 import java.util.Date;
 
 public class Atividade022 {
     public static void Executar()
     {
         SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
-
         Date d = Date.from(Instant.parse("2025-03-22T19:28:07Z"));
+
+        Calendar cal = Calendar.getInstance();
+
+        cal.setTime(d);
+        cal.add(Calendar.HOUR_OF_DAY, 4);
+
+        d = cal.getTime();
 
         System.out.println(d);
     }
