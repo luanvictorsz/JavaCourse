5f2b61475424c8f36fc75aabfd3365cf2e6e49a3
victor <luanvitorlima2017@gmail.com>
2025-04-01 10:16:14 -0300

    formas de definir enumerador

--- src/Desafios10/Program.java
+++ src/Desafios10/Program.java
@@ -5,8 +5,14 @@ import java.util.Date;
 public class Program {
     public static void Executar()
     {
-        Order order = new Order(1080, new Date(), OrderStatus.DELIVERED);
+        Order order = new Order(1080, new Date(), OrderStatus.PENDING_PAYMENT);
 
         System.out.println(order);
+
+
+        // Duas formas de definir o valor da enumeração
+        OrderStatus os1 = OrderStatus.DELIVERED;
+        OrderStatus os2 = OrderStatus.valueOf("DELIVERED");
+
     }
 }
