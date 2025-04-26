e6ccd36b89a4c957370cc8315fc837fa48ef45ed
victor <luanvitorlima2017@gmail.com>
2025-04-25 22:44:19 -0300

    criando classe BussinesAccount com seu construtor, getters e setters

---
+++ src/Desafios11/Entities/BusinessAccount.java
@@ -0,0 +1,22 @@
+package Desafios11.Entities;
+
+public class BusinessAccount extends Account{
+    private double loanLimit;
+
+    public BusinessAccount(){
+
+    }
+
+    public BusinessAccount(Integer number, String holder, Double balance, double loanLimit) {
+        super(number, holder, balance);
+        this.loanLimit = loanLimit;
+    }
+
+    public double getLoanLimit() {
+        return loanLimit;
+    }
+
+    public void setLoanLimit(double loanLimit) {
+        this.loanLimit = loanLimit;
+    }
+}
