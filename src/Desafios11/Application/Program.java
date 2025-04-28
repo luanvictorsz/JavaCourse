cb0ac4db755b5d66314d6d949494f27608640aa1
victor <luanvitorlima2017@gmail.com>
2025-04-27 21:31:48 -0300

    upcasting e dados de conta

--- src/Desafios11/Application/Program.java
+++ src/Desafios11/Application/Program.java
@@ -1,5 +1,12 @@
 package Desafios11.Application;
 
-public class Program {
+import Desafios11.Entities.Account;
+import Desafios11.Entities.BusinessAccount;
 
+public class Program {
+    Account acc = new Account(1001, "Alex", 0.0);
+    BusinessAccount bacc = new BusinessAccount(1002, "Victor", 0.0, 500.0);
+
+    //Upcasting - Atribuindo uma conta empresario para uma conta padrão
+    Account acc1 = bacc;
 }
