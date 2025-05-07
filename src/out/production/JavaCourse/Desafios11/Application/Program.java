d399e51df9290dedb4aacda61eb210022c65201c
L. Victor <luanvitorlima2017@gmail.com>
2025-05-07 10:14:02 -0300

    Sobreposição de metodos

---
+++ src/out/production/JavaCourse/Desafios11/Application/Program.java
@@ -0,0 +1,28 @@
+package Desafios11.Application;
+
+import Desafios11.Entities.Account;
+import Desafios11.Entities.BusinessAccount;
+import Desafios11.Entities.SavingsAccount;
+
+public class Program {
+    /*
+    Account acc = new Account(1001, "Alex", 0.0);
+    BusinessAccount bacc = new BusinessAccount(1002, "Victor", 0.0, 500.0);
+
+    //Upcasting - Atribuindo uma conta empresario para uma conta padrão
+    Account acc1 = bacc;
+    Account acc2 = new BusinessAccount(1003, "Bob", 0.0, 200.0);
+    Account acc3 = new SavingsAccount(1004, "Paula", 0.0, 0.01);
+
+    //DownCasting
+    BusinessAccount acc4 = (BusinessAccount)acc2;
+    
+    //acc4.Loan(100.0);
+
+    BusinessAccount acc5 = (BusinessAccount)acc3; //obs: existe um erro de excessão por conversão por não ser possivel converter para BusinessAccount
+    //acc5.Loan(100.0); */
+
+    Account acc1 = new Account(1001, "Alex", 1000.0);
+
+}
+
