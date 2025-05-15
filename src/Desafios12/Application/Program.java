0df48dee709d78117826b06b741763fb905edb5d
victor <luanvitorlima2017@gmail.com>
2025-05-14 23:08:52 -0300

    iniciando entradas da aplicacao: entrada de nome

--- src/Desafios12/Application/Program.java
+++ src/Desafios12/Application/Program.java
@@ -1,4 +1,30 @@
 package Desafios12.Application;
 
+import java.awt.List;
+import java.util.ArrayList;
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Program {
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc =  new Scanner(System.in);
+		
+		List<Employee> list = new ArrayList<>();
+		
+		System.out.println("Enter the number of employees: ");
+		int n = sc.nextInt();
+		
+		for(int i = 0; i <= n; i++) {
+			System.out.println("Employee #" + i + " data:");
+			System.out.println("outsourced (y/n)?");
+			char ch = sc.next().charAt(0);
+			
+			System.out.print("Name: ");
+			sc.nextLine();
+			String name = sc.nextLine();
+		}
+		
+		sc.close();
+	}
 }
