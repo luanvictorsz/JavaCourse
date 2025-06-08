f984155063071ffef9ecc6babdd714a052cd1761
L. Victor <luanvitorlima2017@gmail.com>
2025-06-08 20:57:36 -0300

    tratamento de exceçao de um erro array

--- src/Atividades/Atividade023.java
+++ src/Atividades/Atividade023.java
@@ -7,12 +7,17 @@ public class Atividade023 {
     {
         Scanner sc = new Scanner(System.in);
 
-        String[] vect = sc.nextLine().split(" ");
-        int position = sc.nextInt();
-        System.out.println(vect[position]);
+        try
+        {
+            String[] vect = sc.nextLine().split(" ");
+            int position = sc.nextInt();
+            System.out.println(vect[position]);
+        }
+        catch (ArrayIndexOutOfBoundsException e)
+        {
+            System.out.println("Fim do programa");
+        }
 
         sc.close();
-
-        System.out.println("Fim do programa");
     }
 }
